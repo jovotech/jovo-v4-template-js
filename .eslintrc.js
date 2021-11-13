ad084bb80e5e4d0ddad04257c4c750b75d0c2331
Alex <a.swetlow@gmail.com>
2021-11-13 14:44:26 +0100

    :bug: Fix eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   root: true,
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
